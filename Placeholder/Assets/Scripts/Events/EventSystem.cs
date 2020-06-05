da89ba31a7c980e89eeb5ea98ad6d794e2563d6b
drew767 <andreylaz@gmail.com>
2020-06-05 13:03:36 +0300

    Add obj folder to git ignore add Event system to game

---
+++ Placeholder/Assets/Scripts/Events/EventSystem.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventSystem : MonoBehaviour
+{
+    private static Dictionary<Type, List<Action<object>>> m_listeneres = new Dictionary<Type, List<Action<object>>>();
+
+    public static void RegisterListener<T>(Action<object> listener) where T : class
+    {
+        if (m_listeneres.ContainsKey(typeof(T)) == false)
+        {
+            m_listeneres.Add(typeof(T), new List<Action<object>>());
+        }
+        m_listeneres[typeof(T)].Add(listener);
+    }
+
+    public static void UnregisterListener<T>(Action<object> listener) where T : class
+    {
+        if (m_listeneres.ContainsKey(typeof(T)) == false)
+        {
+            throw new Exception("There is no such listener");
+        }
+        m_listeneres[typeof(T)].Remove(listener);
+    }
+
+    public static void SendEvent<T>(T incomingEvent) where T : class
+    {
+        if (m_listeneres.ContainsKey(typeof(T)) == false)
+        {
+            return;
+        }
+
+        foreach (var listener in m_listeneres[typeof(T)])
+        {
+            listener.Invoke(incomingEvent);
+        }
+    }
+}
---
+++ Placeholder/Assets/Scripts/Events/Events.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+// Event Example
+//class PauseMenuActivated
+//{
+//}
+//EventSystem.RegisterListener<PauseMenuActivated>(OnPauseRequested);
+// Event calls
+//private void OnPauseRequested(object incomingEvent) {}
+//EventSystem.SendEvent(new PauseMenuActivated() { });
