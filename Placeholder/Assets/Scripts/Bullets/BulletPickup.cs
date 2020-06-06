ddff75fcf8230cd5ec9f10942f9a95ee1c3b2f77
Bohdan Ivashko <bohdan.ivashko@ubisoft.com>
2020-06-06 14:22:43 +0300

    bullet spawn components

---
+++ Placeholder/Assets/Scripts/Bullets/BulletPickup.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletPickup : MonoBehaviour
+{
+    [SerializeField]
+    float m_speed = 1.0f;
+    [SerializeField]
+    float m_pickupDistance = 1.0f;
+    [SerializeField]
+    float m_attractionDistance = 10.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerController player = GameManager.GetInstance().Player;
+        if (!player)
+        {
+            return;
+        }
+        Vector3 distance = player.transform.position - transform.position;
+        if (distance.magnitude < m_pickupDistance)
+        {
+            SendMessage("BulletPickup", player, SendMessageOptions.DontRequireReceiver);
+            Destroy(this.gameObject);
+        }
+        else if (distance.magnitude < m_attractionDistance)
+        {
+            Vector3 direction = distance.normalized;
+            direction = new Vector3(direction.x, 0.0f, direction.z);
+            Rigidbody rb = GetComponent<Rigidbody>();
+
+            rb.velocity = direction * m_speed * Time.deltaTime * 10.0f;
+        }
+    }
+}
---
+++ Placeholder/Assets/Scripts/Enemies/LootComponent.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LootComponent : MonoBehaviour
+{
+    public GameObject loot;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnStateChanged(EnemyController enemy)
+    {
+        if (enemy.State == eState.DEAD)
+        {
+            GameObject instFoam = Instantiate(foam, transform.position, Quaternion.identity);
+        }
+    }
+}
