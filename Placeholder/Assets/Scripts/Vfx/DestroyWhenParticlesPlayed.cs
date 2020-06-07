f0553c1fe91b4bebbd253614d17b2e1895b97d9a
ibatalkin <iurii.batalkin@gmail.com>
2020-06-07 03:18:27 +0300

    Water rifle bullet vfx

---
+++ Placeholder/Assets/Scripts/Vfx/DestroyWhenParticlesPlayed.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyWhenParticlesPlayed : MonoBehaviour
+{
+    [SerializeField]
+    List<ParticleSystem> m_particles = new List<ParticleSystem>();
+
+    void Update()
+    {
+        int aliveParticleSystems = 0;
+        foreach(var particle in m_particles)
+		{
+            if(particle.IsAlive())
+			{
+                ++aliveParticleSystems;
+            }
+		}
+
+        if(aliveParticleSystems != m_particles.Count)
+		{
+            Destroy(gameObject);
+		}
+    }
+}
